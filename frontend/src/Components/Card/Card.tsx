2aeff0c755980801892189e1a1ae99a5594b7d79
poluiza <depaulaoliveiraluiza@gmail.com>
2026-04-30 00:24:23 -0300

    Aula 12: Forms

--- frontend/src/Components/Card/Card.tsx
+++ frontend/src/Components/Card/Card.tsx
@@ -1,23 +1,24 @@
-import React, { JSX } from "react";
+import React, { JSX, SyntheticEvent } from "react";
 import './Card.css';
 import { CompanySearch } from "../../company";
+import AddPortfolio from "../Portfolio/AddPortfolio";
 
 interface Props {
-    id: String;
+    id: string;
     searchResult: CompanySearch;
+    onPortfolioCreate: (e: SyntheticEvent) => void;
 }
 
-const Card: React.FC<Props> = ({id, searchResult}: Props) : JSX.Element => {
+const Card: React.FC<Props> = ({id, searchResult, onPortfolioCreate}: Props) : JSX.Element => {
     return (
-        <div className="card">
-            
+        <div key={id} id={id} className="card">
             <img alt="company logo"/>
             <div className="details"> 
                 <h2>{searchResult.name} ({searchResult.symbol})</h2> 
                 <p>${searchResult.currency}</p>
             </div>
             <p className="info">{searchResult.exchangeShortName} - {searchResult.stockExchange}</p>
-        
+        <AddPortfolio onPortfolioCreate={onPortfolioCreate} symbol={searchResult.symbol}/>
         </div>
     )
 }
--- frontend/src/Components/Search/Search.tsx
+++ frontend/src/Components/Search/Search.tsx
@@ -6,22 +6,14 @@ interface Props {
   handleSearchChange: (e: ChangeEvent<HTMLInputElement>) => void;
 }
 
-const Search: React.FC<Props> = ({onSearchSubmit, handleSearchChange}: Props) : JSX.Element=> {
-    const [search,setSearch] = useState<string>("");
-
-    const handleChange = (e: ChangeEvent<HTMLInputElement>) =>{
-        setSearch(e.target.value);
-        console.log(e);
-    };
-    const onClick = (e: SyntheticEvent) => {
-        console.log(e);
-    };
+const Search: React.FC<Props> = ({onSearchSubmit, handleSearchChange, search}: Props) : JSX.Element=> {
     return (
-        <div>
-            <input value = {search} onChange={(e) => handleChange(e)} type="text" />
-            <button onClick={(e) => onClick(e)}/>
-        </div>
-    )
+      <>
+      <form onSubmit={onSearchSubmit}>
+        <input value={search} onChange={handleSearchChange} />
+      </form>
+      </>
+    );
 }
 
 export default Search
