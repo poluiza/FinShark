7623449e59d2db8b9b184e2717d7dd948eb8063c
poluiza <depaulaoliveiraluiza@gmail.com>
2026-05-01 23:01:40 -0300

    Aula 16: React Router

---
+++ frontend/src/Pages/SearchPage/SearchPage.tsx
@@ -0,0 +1,79 @@
+import React, { ChangeEvent, SyntheticEvent, useState } from "react";
+import { CompanySearch } from "../../company";
+import { searchComponies } from "../../api";
+import Navbar from "../../Components/Navbar/Navbar";
+import Search from "../../Components/Search/Search";
+import ListPortfolio from "../../Components/Portfolio/ListPortfolio/ListPortfolio";
+import CardList from "../../Components/CardList/CardList";
+
+interface Props { }
+
+const SearchPage = (props: Props) => {
+    const [search, setSearch] = useState<string>("");
+    const [PortfolioValues, setPortfolioValues] = useState<string[]>([]);
+    const [searchResult, setSearchResult] = useState<CompanySearch[]>([]);
+    const [serverError, setServerError] = useState<string | null>(null);
+
+    const handleSearchChange = (e: ChangeEvent<HTMLInputElement>) => {
+        setSearch(e.target.value);
+        console.log(e);
+    };
+
+    const onPortfolioCreate = (e: any) => {
+        e.preventDefault();
+        const exists = PortfolioValues.find((value) => value === e.target[0].value)
+        if (exists) return;
+        const updatePortfolio = [...PortfolioValues, e.target[0].value];
+        setPortfolioValues(updatePortfolio);
+    }
+
+    const onPortfolioDelete = (e: any) => {
+        e.preventDefault();
+        const removed = PortfolioValues.filter((value) => {
+            return value !== e.target[0].value;
+        });
+        setPortfolioValues(removed);
+    };
+
+    const onSearchSubmit = async (e: SyntheticEvent) => {
+        e.preventDefault();
+        const result = await searchComponies(search);
+
+        if (typeof result === "string") {
+            setServerError(result);
+        }
+
+        else if (Array.isArray(result)) {
+            setSearchResult(result);
+        }
+
+        console.log(searchResult);
+    };
+
+
+    return (
+        <div className='App'>
+
+            <Search
+                onSearchSubmit={onSearchSubmit}
+                search={search}
+                handleSearchChange={handleSearchChange}
+            />
+
+            <ListPortfolio
+                portfolioValues={PortfolioValues}
+                onPortfolioDelete={onPortfolioCreate}
+            />
+
+            <CardList
+                searchResults={searchResult}
+                onPortfolioCreate={onPortfolioCreate}
+            />
+
+            {serverError && <div> Unable to connect to API</div>}
+
+        </div>
+    )
+}
+
+export default SearchPage
