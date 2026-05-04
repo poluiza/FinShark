4ac7fde90d362e6ad7d78947c555ebfb06c89344
poluiza <depaulaoliveiraluiza@gmail.com>
2026-05-04 16:19:36 -0300

    Aula 17: useEffect

--- frontend/src/api.tsx
+++ frontend/src/api.tsx
@@ -1,29 +1,34 @@
-import axios from "axios"
-import { CompanySearch } from "./company"
-import { error } from "console";
+import axios from "axios";
+import { CompanyProfile, CompanySearch } from "./company";
 
-interface SearchResponse {
-    data: CompanySearch[];
+export interface SearchResponse {
+  data: CompanySearch[];
 }
 
-export const searchComponies = async (query: string) => {
-    try {
-        const data = await axios.get<SearchResponse>(
-            `https://api.twelvedata.com/symbol_search?symbol=${query}&apikey=${process.env.REACT_APP_API_KEY}`
-        );
-        return data.data.data;
+export const searchCompanies = async (query: string) => {
+  try {
+    const data = await axios.get<SearchResponse>(
+      `https://api.twelvedata.com/symbol_search?symbol=${query}&apikey=${process.env.REACT_APP_API_KEY}`
+    );
+    return data.data.data;
+  } catch (error) {
+    if (axios.isAxiosError(error)) {
+      console.log("error message: ", error.message);
+      return error.message;
+    } else {
+      console.log("unexpected error: ", error);
+      return "An expected error has occured.";
     }
+  }
+};
 
-    catch (error) {
-
-        if (axios.isAxiosError(error)) {
-            console.log("Error message: ", error.message);
-            return error.message;
-        }
-
-        else {
-            console.log("unexpected error: ", error);
-        return "An expected error has accured. | Ocorreu um erro esperado.";
-        }
-    }
-}
+export const getCompanyProfile = async (query: string) => {
+  try {
+    const data = await axios.get<CompanyProfile[]>(
+      `https://financialmodelingprep.com/api/v3/profile/${query}?apikey=${process.env.REACT_APP_API_KEY}`
+    );
+    return data;
+  } catch (error: any) {
+    console.log("error message: ", error.message);
+  }
+};
