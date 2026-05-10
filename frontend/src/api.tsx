2c7dfbbc299733abcde720b15ae49a6c5eba98d9
poluiza <depaulaoliveiraluiza@gmail.com>
2026-05-10 20:47:14 -0300

    Aula 24: Cash Flow Statement

--- frontend/src/api.tsx
+++ frontend/src/api.tsx
@@ -2,6 +2,7 @@ import axios from "axios";
 import {
   CompanyBalanceSheet,
   CompanyIncomeStatement,
+  CompanyCashFlow,
   CompanyKeyMetrics,
   CompanyProfile,
   CompanySearch,
@@ -62,6 +63,16 @@ export const getBalanceSheet = async (query: string) => {
   try {
     const data = await axios.get<CompanyBalanceSheet[]>(
       `https://financialmodelingprep.com/api/v3/balance-sheet-statement/${query}?limit=20&apikey=${process.env.REACT_APP_API_KEY}`);
+    return data;
+  } catch (error: any) {
+    console.log("error message: ", error.message);
+  }
+};
+export const getCashFlow = async (query: string) => {
+  try {
+    const data = await axios.get<CompanyCashFlow[]>(
+      `https://financialmodelingprep.com/api/v3/cash-flow-statement/${query}?limit=100&apikey=${process.env.REACT_APP_API_KEY}`
+    );
     return data;
   } catch (error: any) {
     console.log("error message: ", error.message);
