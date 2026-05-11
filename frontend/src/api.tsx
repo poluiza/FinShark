4de9edb9e16fdc2f89107f7daeee7f2a7c23c1cd
poluiza <depaulaoliveiraluiza@gmail.com>
2026-05-10 22:51:00 -0300

    Aula 28: Finishing Touches Before API

--- frontend/src/api.tsx
+++ frontend/src/api.tsx
@@ -7,7 +7,9 @@ import {
   CompanyKeyMetrics,
   CompanyProfile,
   CompanySearch,
-   CompanyTenK,
+  CompanyTenK,
+  CompanyHistoricalDividend,
+  Dividend,
 } from "./company";
 export interface SearchResponse {
   data: CompanySearch[];
@@ -43,7 +45,7 @@ export const getCompanyProfile = async (query: string) => {
 export const getKeyMetrics = async (query: string) => {
   try {
     const data = await axios.get<CompanyKeyMetrics[]>(
-         `https://financialmodelingprep.com/api/v3/key-metrics-ttm/${query}?limit=40&apikey=${process.env.REACT_APP_API_KEY}`
+      `https://financialmodelingprep.com/api/v3/key-metrics-ttm/${query}?limit=40&apikey=${process.env.REACT_APP_API_KEY}`
     );
     return data;
   } catch (error: any) {
@@ -96,6 +98,16 @@ export const getTenK = async (query: string) => {
   try {
     const data = await axios.get<CompanyTenK[]>(
       `https://financialmodelingprep.com/api/v3/sec_filings/${query}?type=10-K&page=0&apikey=${process.env.REACT_APP_API_KEY}`
+    );
+    return data;
+  } catch (error: any) {
+    console.log("error message: ", error.message);
+  }
+};
+export const getHistoricalDividend = async (query: string) => {
+  try {
+    const data = await axios.get<CompanyHistoricalDividend>(
+      `https://financialmodelingprep.com/api/v3/historical-price-full/stock_dividend/${query}?apikey=${process.env.REACT_APP_API_KEY}`
     );
     return data;
   } catch (error: any) {
