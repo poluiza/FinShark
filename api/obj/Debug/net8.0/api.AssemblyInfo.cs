d4fe9d2c445a2644beb05a36d9186078d65d5734
poluiza <depaulaoliveiraluiza@gmail.com>
2026-06-02 16:45:58 -0300

    Aula 52: Login

--- api/obj/Debug/net8.0/api.AssemblyInfo.cs
+++ api/obj/Debug/net8.0/api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9604d355c2a54a3eb338d5a48ec991826fb02188")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8c78279c34800776c575d914726b43853b70f7c8")]
 [assembly: System.Reflection.AssemblyProductAttribute("api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
