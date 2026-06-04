49930b5532e84d3372be39462509dbe435ed80a2
poluiza <depaulaoliveiraluiza@gmail.com>
2026-06-04 16:35:34 -0300

    Aula 59: Data Seeding

--- api/obj/Debug/net8.0/api.AssemblyInfo.cs
+++ api/obj/Debug/net8.0/api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b42284c65fbd745c4300e5fe971500bdb5678ac3")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ee7136c20dadde8dde63995ab570735356cebc2f")]
 [assembly: System.Reflection.AssemblyProductAttribute("api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
